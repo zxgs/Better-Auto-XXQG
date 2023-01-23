c838e74289054c4946336afcb9fbd1eeb08c9156
zxgs <65719131+zxgs@users.noreply.github.com>
2023-01-23 14:40:11 +0800

    Update UI.js

--- UI.js
+++ UI.js
@@ -14,7 +14,7 @@ ui.layout(
     <drawer id="drawer">
         <vertical>
             <appbar>
-                <toolbar id="toolbar" bg="#ff4fb3ff" title={app.getAppName("cc.zxgs.lrzs")}/>
+                <toolbar id="toolbar" bg="#ff4fb3ff" title={app.name("cc.zxgs.lrzs")}/>
                 <tabs id="tabs" bg="#ff4fb3ff"/>
             </appbar>
             <viewpager id="viewpager">
